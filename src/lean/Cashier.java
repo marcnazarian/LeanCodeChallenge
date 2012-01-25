f99da82ccac2b670c99e4e522e4e8cd0b447af16
Marc Nazarian <marc.nazarian@gmail.com>
2012-01-25 20:32:12 +0100

    iteration 1

--- src/lean/Cashier.java
+++ src/lean/Cashier.java
@@ -6,9 +6,34 @@ import java.io.PrintWriter;
 
 public class Cashier {
 
+    private int totalPrice;
 
-    public void checkout(BufferedReader reader, PrintWriter writer) throws IOException {
-        while (true ) 
-            writer.println(reader.readLine());
+	public Cashier() {
+		totalPrice = 0;
+	}
+
+	public void checkout(BufferedReader reader, PrintWriter writer) throws IOException {
+    	while (true ) {
+    		scanItemAndReturnTotalPrice(reader.readLine());
+        	writer.println(totalPrice);
+        }
     }
+
+	public int scanItemAndReturnTotalPrice(String item) {
+		totalPrice += scanItem(item);
+		return totalPrice;
+	}
+
+	public int scanItem(String item) {
+		int itemPrice = 0;
+		if ("Apples".equals(item)) {
+			itemPrice = 100;
+		} else if ("Cherries".equals(item)) {
+			itemPrice = 75;
+		} else if ("Bananas".equals(item)) {
+			itemPrice = 150;
+		}
+		return itemPrice;
+	}
+    
 }
--- test/lean/CashierTest.java
+++ test/lean/CashierTest.java
@@ -7,8 +7,17 @@ import org.junit.Test;
 public class CashierTest {
 
     @Test
-    public void test() {
-        fail("Not yet implemented");
+    public void iteration_1() {
+    	Cashier cashier = new Cashier();
+        assertEquals(100, cashier.scanItemAndReturnTotalPrice("Apples"));
+        assertEquals(175, cashier.scanItemAndReturnTotalPrice("Cherries"));
+        assertEquals(250, cashier.scanItemAndReturnTotalPrice("Cherries"));
+    }
+    
+    @Test
+    public void iteration_1_withBananas() {
+    	Cashier cashier = new Cashier();
+        assertEquals(150, cashier.scanItemAndReturnTotalPrice("Bananas"));
     }
 
 }
